97076eb15217caa29e931f2f452866e7941c4734
Mike Coletta <coletta.mike@gmail.com>
2021-02-28 18:17:20 -0800

    Added custom emails to each test.

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -3,28 +3,28 @@ const Employee = require('../lib/Employee');
 describe('Employee', () => {
     describe('getName', () => {
         it('should return the name of the employee', () => {
-            const Greg = new Employee('Greg', 10, 'greg@business.com', 'Employee');
+            const Greg = new Employee('Greg', 10, 'GregtheEmployee@business.com', 'Employee');
             const gregName = 'Greg';
             expect(Greg.getName()).toBe(gregName);
         })
     })
     describe('getID', () => {
         it('should return the ID of the employee', () => {
-            const Greg = new Employee('Greg', 10, 'greg@business.com', 'Employee');
+            const Greg = new Employee('Greg', 10, 'GregtheEmployee@business.com', 'Employee');
             const gregID = 10;
             expect(Greg.getID()).toBe(gregID);
         })
     })
     describe('getEmail', () => {
         it('should return the email of the employee', () => {
-            const Greg = new Employee('Greg', 10, 'greg@business.com', 'Employee');
-            const gregEmail = 'greg@business.com';
+            const Greg = new Employee('Greg', 10, 'GregtheEmployee@business.com', 'Employee');
+            const gregEmail = 'GregtheEmployee@business.com';
             expect(Greg.getEmail()).toBe(gregEmail);
         })
     })
     describe('getRole', () => {
         it('should return the Role of the employee', () => {
-            const Greg = new Employee('Greg', 10, 'greg@business.com', 'Employee');
+            const Greg = new Employee('Greg', 10, 'GregtheEmployee@business.com', 'Employee');
             const gregRole = 'Employee';
             expect(Greg.getRole()).toBe(gregRole);
         })
